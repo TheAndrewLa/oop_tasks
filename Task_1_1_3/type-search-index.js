7ae332b3609070c01b290b300e918814c71b322b
TheAndrewLa <TheAndrewLa@users.noreply.github.com>
2024-10-08 06:03:26 +0000

    Deploying to gh-pages from @ TheAndrewLa/oop_tasks@ecc1d29eac1733bb9a075c5645f8715101b0703d 🚀

---
+++ Task_1_1_3/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"andrewla","l":"Add"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"andrewla","l":"BinaryExpression"},{"p":"andrewla","l":"Div"},{"p":"andrewla","l":"Expression"},{"p":"andrewla","l":"Mul"},{"p":"andrewla","l":"Number"},{"p":"andrewla","l":"Sub"},{"p":"andrewla","l":"Variable"}]
