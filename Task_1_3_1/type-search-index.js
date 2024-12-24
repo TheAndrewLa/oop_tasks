8499204ce51ed40b600054a4539cc2b63b6d5e33
TheAndrewLa <TheAndrewLa@users.noreply.github.com>
2024-12-24 04:04:52 +0000

    Deploying to gh-pages from @ TheAndrewLa/oop_tasks@b21ead30e94b61964f516b1e1b42ea656791db21 🚀

---
+++ Task_1_3_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"andrewla","l":"BufferedFileReader"},{"p":"andrewla","l":"SubstringFind"}];updateSearchResults();
